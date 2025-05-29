435ea2851796236ea311cf1737863e706a8ef6c3
TharlisSampaio <tharlis.sampaio@outlook.com>
2025-05-29 09:44:58 -0300

    feat - definindo a entidade agenda em model

---
+++ src/main/java/com/evo/agenda/model/Agenda.java
@@ -0,0 +1,45 @@
+package com.evo.agenda.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "agenda")
+public class Agenda {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String nome;
+
+    private String sobrenome;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    
+}
